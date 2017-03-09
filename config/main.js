cfbdb692d309824e84b74b010f767d0d03f1951f
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-03-09 22:00:38 +1100

    Readme file added

--- config/main.js
+++ config/main.js
@@ -2,7 +2,8 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'super secret passphrase',
   // Database connection information
-  'database': 'mongodb://heroku_jsgfv3wj:tctof8mku69t2bnepcgb5vvt0k@ds123370.mlab.com:23370/heroku_jsgfv3wj',
+  'database': 'mongodb://heroku_jzsq09w7:abcd1234@ds123400.mlab.com:23400/heroku_jzsq09w7',
   // Setting port for server
   'port': process.env.PORT || 3000    
 }
+
