505b246a4f09e1a8f4fa5f67be038e7071608994
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-03-09 20:10:52 +1100

    Added horku config

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'super secret passphrase',
   // Database connection information
-  'database': 'mongodb://localhost:27017/',
+  'database': 'mongodb://heroku_jsgfv3wj:tctof8mku69t2bnepcgb5vvt0k@ds123370.mlab.com:23370/heroku_jsgfv3wj',
   // Setting port for server
   'port': process.env.PORT || 3000    
 }
