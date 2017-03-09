4067d656fd59964242d2fa71a0bd1d1c1fdcdd91
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-03-09 22:13:49 +1100

    Mongo db connection added

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'super secret passphrase',
   // Database connection information
-  'database': 'mongodb://heroku_jzsq09w7:abcd1234@ds123400.mlab.com:23400/heroku_jzsq09w7',
+  'database': 'mongodb://heroku_zhbzbh4l:u25dfj6h9jkha7hcmcaj4pl41u@ds123370.mlab.com:23370/heroku_zhbzbh4l',    
   // Setting port for server
   'port': process.env.PORT || 3000    
 }
